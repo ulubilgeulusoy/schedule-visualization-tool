e1efedeed48ac1cf938351faa8c7f25814afe61b
ulubilgeulusoy <ulusoy@usc.edu>
2026-07-02 14:33:25 -0600

    Folder structure updated

---
+++ firebase-config.example.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "YOUR_FIREBASE_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.firebasestorage.app",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_FIREBASE_APP_ID"
+};
+
+export const campaignDocPath = ["campaigns", "adapt2-2026"];
---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDdtJlVRDXr8x3k9GCfOzGpOhGqvidkiu8",
+  authDomain: "adapt2-schedule.firebaseapp.com",
+  projectId: "adapt2-schedule",
+  storageBucket: "adapt2-schedule.firebasestorage.app",
+  messagingSenderId: "235290453657",
+  appId: "1:235290453657:web:3bf4328a890eba9174fa08"
+};
+
+export const campaignDocPath = ["campaigns", "adapt2-2026"];
